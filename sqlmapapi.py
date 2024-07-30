f98c84a8e69077fb0a93a6c41e9591a3c000487f
TechGuru-521 <remigarton762@hotmail.com>
2024-07-30 17:23:15 +0300

    Update sqlmapapi.py

--- sqlmapapi.py
+++ sqlmapapi.py
@@ -1,66 +1,43 @@
 #!/usr/bin/env python
 
-"""
-Copyright (c) 2006-2019 sqlmap developers (http://sqlmap.org/)
-See the file 'LICENSE' for copying permission
-"""
-
 import sys
-
-sys.dont_write_bytecode = True
-
-__import__("lib.utils.versioncheck")  # this has to be the first non-standard import
-
 import logging
 import optparse
 import warnings
 
-warnings.filterwarnings(action="ignore", message=".*was already imported", category=UserWarning)
-warnings.filterwarnings(action="ignore", category=DeprecationWarning)
+sys.dont_write_bytecode = True
 
-from sqlmap import modulePath
+# Import necessary modules
 from lib.core.common import setPaths
 from lib.core.data import logger
-from lib.core.patch import dirtyPatches
-from lib.core.patch import resolveCrossReferences
-from lib.core.settings import RESTAPI_DEFAULT_ADAPTER
-from lib.core.settings import RESTAPI_DEFAULT_ADDRESS
-from lib.core.settings import RESTAPI_DEFAULT_PORT
-from lib.utils.api import client
-from lib.utils.api import server
+from lib.core.patch import dirtyPatches, resolveCrossReferences
+from lib.core.settings import RESTAPI_DEFAULT_ADAPTER, RESTAPI_DEFAULT_ADDRESS, RESTAPI_DEFAULT_PORT
+from lib.utils.api import client, server
 
 def main():
-    """
-    REST-JSON API main function
-    """
-
     dirtyPatches()
     resolveCrossReferences()
 
-    # Set default logging level to debug
     logger.setLevel(logging.DEBUG)
-
-    # Initialize paths
     setPaths(modulePath())
 
-    # Parse command line options
-    apiparser = optparse.OptionParser()
-    apiparser.add_option("-s", "--server", help="Run as a REST-JSON API server", default=RESTAPI_DEFAULT_PORT, action="store_true")
-    apiparser.add_option("-c", "--client", help="Run as a REST-JSON API client", default=RESTAPI_DEFAULT_PORT, action="store_true")
-    apiparser.add_option("-H", "--host", help="Host of the REST-JSON API server (default \"%s\")" % RESTAPI_DEFAULT_ADDRESS, default=RESTAPI_DEFAULT_ADDRESS, action="store")
-    apiparser.add_option("-p", "--port", help="Port of the the REST-JSON API server (default %d)" % RESTAPI_DEFAULT_PORT, default=RESTAPI_DEFAULT_PORT, type="int", action="store")
-    apiparser.add_option("--adapter", help="Server (bottle) adapter to use (default \"%s\")" % RESTAPI_DEFAULT_ADAPTER, default=RESTAPI_DEFAULT_ADAPTER, action="store")
-    apiparser.add_option("--username", help="Basic authentication username (optional)", action="store")
-    apiparser.add_option("--password", help="Basic authentication password (optional)", action="store")
-    (args, _) = apiparser.parse_args()
+    parser = optparse.OptionParser()
+    parser.add_option("-s", "--server", help="Run as a REST-JSON API server", default=False, action="store_true")
+    parser.add_option("-c", "--client", help="Run as a REST-JSON API client", default=False, action="store_true")
+    parser.add_option("-H", "--host", help=f"Host of the REST-JSON API server (default \"{RESTAPI_DEFAULT_ADDRESS}\")", default=RESTAPI_DEFAULT_ADDRESS)
+    parser.add_option("-p", "--port", help=f"Port of the REST-JSON API server (default {RESTAPI_DEFAULT_PORT})", default=RESTAPI_DEFAULT_PORT, type="int")
+    parser.add_option("--adapter", help=f"Server (bottle) adapter to use (default \"{RESTAPI_DEFAULT_ADAPTER}\")", default=RESTAPI_DEFAULT_ADAPTER)
+    parser.add_option("--username", help="Basic authentication username (optional)")
+    parser.add_option("--password", help="Basic authentication password (optional)")
 
-    # Start the client or the server
-    if args.server is True:
-        server(args.host, args.port, adapter=args.adapter, username=args.username, password=args.password)
-    elif args.client is True:
-        client(args.host, args.port, username=args.username, password=args.password)
+    options, _ = parser.parse_args()
+
+    if options.server:
+        server(options.host, options.port, adapter=options.adapter, username=options.username, password=options.password)
+    elif options.client:
+        client(options.host, options.port, username=options.username, password=options.password)
     else:
-        apiparser.print_help()
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
